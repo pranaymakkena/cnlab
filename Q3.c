cdddfb5dca1a414e889ea9fba95adefbcc365ecc
Pranay Makkena <pranaymakkena@gmail.com>
2026-08-18 11:42:55 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 int main() {
-    int frames, window, i;
+    int frames, window, lost, i, j;
 
     printf("Enter number of frames: ");
     scanf("%d", &frames);
@@ -14,14 +14,54 @@ int main() {
     printf("Enter window size: ");
     scanf("%d", &window);
 
-    for (i = 0; i < frames; i++) {
-        printf("Sending frame %d\n", i);
+    printf("Enter frame number to simulate loss: ");
+    scanf("%d", &lost);
 
-        if (i % window == 0) {
-            printf("Acknowledgement received up to frame %d\n", i);
+    printf("\n--- Go-Back-N Sliding Window Simulation ---\n\n");
+
+    i = 0;
+
+    while (i < frames) {
+        printf("Sending window: ");
+
+        for (j = i; j < i + window && j < frames; j++) {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+
+        for (j = i; j < i + window && j < frames; j++) {
+
+            if (j == lost) {
+                printf("Frame %d lost!\n", j);
+                printf("Negative acknowledgement for frame %d\n", j);
+                printf("Go-Back-N: Retransmitting from frame %d\n\n", j);
+
+                /* Retransmit lost frame and remaining frames */
+                for (int k = j; k < i + window && k < frames; k++) {
+                    printf("Retransmitting frame %d\n", k);
+                }
+
+                printf("Acknowledgement received up to frame %d\n\n",
+                       (i + window < frames) ? i + window - 1 : frames - 1);
+
+                i += window;
+                break;
+            } 
+            else {
+                printf("Frame %d received successfully\n", j);
+            }
+        }
+
+        if (lost < i || lost >= i + window) {
+            printf("Acknowledgement received up to frame %d\n\n",
+                   (i + window < frames) ? i + window - 1 : frames - 1);
+
+            i += window;
         }
     }
 
-    printf("Go-Back-N simulation complete\n");
+    printf("Go-Back-N simulation complete.\n");
+
     return 0;
 }
